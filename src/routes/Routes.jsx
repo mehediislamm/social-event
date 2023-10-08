e1087a67a61f5f1be2356a6fa6003ccf43512cd0
mehedi505991 <mdmehediislammoksedul@gmail.com>
2023-10-08 19:57:22 +0600

    add-the-service-section-for-with-proviceRoute

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
             },
             {
                 path:'/servic',
-                element:<Service></Service>
+                element:<PrivateRoute><Service></Service></PrivateRoute>
             },
             {
                 path:'/login',
